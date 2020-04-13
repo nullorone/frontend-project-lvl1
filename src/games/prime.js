7ccdbc1dee20e4158c67e2cb1dba2cf4cac594bb
ysveshnikov <ysveshnikov@ivi.ru>
2020-04-13 21:07:55 +0300

    Исправляет нейминг

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,13 +4,13 @@ import generateNumberOfRange from '../utils.js';
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (value) => {
-  const limitIterator = Math.floor(Math.sqrt(value));
-
   if (value <= 1) {
     return false;
   }
 
-  for (let i = 2; i <= limitIterator; i += 1) {
+  const maxDivider = Math.floor(Math.sqrt(value));
+
+  for (let i = 2; i <= maxDivider; i += 1) {
     if (value % i === 0) {
       return false;
     }
