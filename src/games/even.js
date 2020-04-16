178f598f0f6bf8f3b251727826e3ac8a1ec10a95
ysveshnikov <ysveshnikov@ivi.ru>
2020-04-16 10:39:59 +0300

    Удалена лишняя строка

--- src/games/even.js
+++ src/games/even.js
@@ -6,7 +6,6 @@ const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 // Return true if number even
 const isEven = (number) => number % 2 === 0;
 
-
 const getQuestionAnswer = () => {
   const question = generateNumberOfRange(1, 100);
 
